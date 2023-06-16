e4fa63e79d9182a65be65f372ba907388e63887f
yws <1349951108@qq.com>
2023-06-16 13:17:07 +0800

    毕设后台

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  productionSourceMap:false,
+  transpileDependencies: true,
+  publicPath: './',
+  //关闭eslint语法检查
+  lintOnSave:false,
+  //代理跨域
+  devServer:{
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:3000",
+        changeOrigin: true,
+        // pathRewrite: {"^/api" : ""}
+     }
+    }
+  }
 })
